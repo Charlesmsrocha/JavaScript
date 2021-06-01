c2efd2a5f08a179b59df3cb7b82e1cfd09e967a8
Charles Rocha <charlesmsrocha@gmail.com>
2021-06-01 17:41:08 +0100

    Files name
    change the files names and dividing the code.

---
+++ pratice/pratice004anonym.js
@@ -0,0 +1,8 @@
+var anon1 = function (num1){
+    num1 = Math.round(num1);
+    alert(num1);
+};
+
+//alert(x());
+
+anon1(25.56);
---
+++ pratice/pratice018.js
@@ -0,0 +1,11 @@
+a = false;
+b = new Boolean(false);
+document.write(typeof a);
+document.write(typeof b);
+
+c = Boolean(2>5);
+d = Boolean(0);
+
+alert("c is " + c);
+alert("d is " + d);
+document.write("<BR>" + c.toString());
